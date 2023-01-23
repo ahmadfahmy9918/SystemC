43cd2d177a8ca1514af878268af98b15f9e59e86
Ahmad Fahmy <ahmadfahmy9918@gmail.com>
2023-01-22 22:34:26 -0500

    Update ALU.cpp

--- ALUnBarrelshift/ALU.cpp
+++ ALUnBarrelshift/ALU.cpp
@@ -3,14 +3,6 @@
 #include "ALU.h"
 
 void ALU :: ALU_method(){
-	//sc_int<8> A, B;
-	//sc_in<int> A, B;
-	//sc_in_clk clock;
-	//sc_in<bool> OP;
-	//int i;
-
-	//sc_out<int> alu_out;
-
 	if(OP.read() == 1){ //addition
 		alu_out.write(A.read()+B.read());		
 	}
